6c53536fabb85cb78243b2d96f19c176987faa52
Saranga Rajagopalan <sarnga.raj@gmail.com>
2021-08-11 15:55:29 -0400

    Drive constructor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/v1/fishlo/robot/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/v1/fishlo/robot/Drive.java
@@ -1,11 +1,21 @@
 package org.firstinspires.ftc.teamcode.fishlo.v1.fishlo.robot;
 
+import org.firstinspires.ftc.teamcode.robot.Robot;
 import org.firstinspires.ftc.teamcode.robot.SubSystem;
 
 public class Drive extends SubSystem {
+    /**
+     * Construct a subsystem with the robot it applies to.
+     *
+     * @param robot
+     */
+    public Drive(Robot robot) {
+        super(robot);
+    }
+
     @Override
     public void init() {
-        
+
     }
 
     @Override
