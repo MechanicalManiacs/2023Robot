cb848980314af79a06af77fe277109fa9d6859f6
Saranga Rajagopalan <sarnga.raj@gmail.com>
2021-10-11 11:05:16 -0400

    telop fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/v1/fishlo/robot/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/v1/fishlo/robot/Drive.java
@@ -1,7 +1,11 @@
 package org.firstinspires.ftc.teamcode.fishlo.v1.fishlo.robot;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.arcrobotics.ftclib.drivebase.HDrive;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.robot.Robot;
@@ -9,9 +13,13 @@ import org.firstinspires.ftc.teamcode.robot.SubSystem;
 
 public class Drive extends SubSystem {
 
-    private DcMotor frontLeft, frontRight, backLeft, backRight;
+    private Motor frontLeft, frontRight, backLeft, backRight;
+
+    HDrive xDrive;
 
     SampleMecanumDrive mecanumDrive = new SampleMecanumDrive(robot.hardwareMap);
+
+    Servo claw;
 
     private enum DriveControls {
         TANK,
@@ -32,40 +40,38 @@ public class Drive extends SubSystem {
 
     @Override
     public void init() {
-        frontLeft = robot.hardwareMap.dcMotor.get("frontLeft");
-        frontRight = robot.hardwareMap.dcMotor.get("fronRight");
-        backLeft = robot.hardwareMap.dcMotor.get("backLeft");
-        backRight = robot.hardwareMap.dcMotor.get("backRight");
+        frontLeft = new Motor(robot.hardwareMap, "frontLeft");
+        frontRight = new Motor(robot.hardwareMap, "frontRight");
+        backLeft = new Motor(robot.hardwareMap, "backLeft");
+        backRight = new Motor(robot.hardwareMap, "backRight");
+        claw = robot.hardwareMap.servo.get("claw");
+        xDrive = new HDrive(frontLeft, frontRight, backLeft, backRight);
 
         robot.telemetry.addLine("Motors initialized");
     }
 
     @Override
     public void handle() {
-        double driveSpeed = -robot.gamepad1.left_stick_y / 3;
-        double rightY = robot.gamepad1.right_stick_y / 3;
+        double driveSpeed = robot.gamepad1.left_stick_y / 4;
         double turnSpeed = 0;
-        double strafeSpeed = robot.gamepad1.left_stick_x / 3;
 
-
-        if (Math.abs(robot.gamepad1.right_stick_x) > 0.1) {
-            turnSpeed = robot.gamepad1.right_stick_x / 3;
-        }
+        turnSpeed = robot.gamepad1.right_stick_x / 4;
 
         driveIndex = 1;
 
         driveType = driveControls[driveIndex];
-        runDrive(driveType, driveSpeed, strafeSpeed, turnSpeed, rightY, -driveSpeed);
+        if (robot.gamepad1.y) {
+            claw.setPosition(0.5);
+        }
+        if (robot.gamepad1.a) {
+            claw.setPosition(0);
+        }
+        xDrive.driveRobotCentric(-robot.gamepad1.left_stick_x, driveSpeed , turnSpeed );
 
         robot.telemetry.addData("Drive - Dat - Drive Controls", driveType.name());
         robot.telemetry.addData("Drive - Dat - Drive Speed", driveSpeed);
         robot.telemetry.addData("Drive - Dat - Turn Speed", turnSpeed);
         robot.telemetry.addData("Drive - Dat - GamepadX", robot.gamepad1.left_stick_x);
-        robot.telemetry.addData("Drive - Dat - Strafe Speed", strafeSpeed);
-        robot.telemetry.addData("Drive - Set - frontLeft", frontLeft.getPower());
-        robot.telemetry.addData("Drive - Set - backLeft", backLeft.getPower());
-        robot.telemetry.addData("Drive - Set - frontRight", frontRight.getPower());
-        robot.telemetry.addData("Drive - Set - backRight", backRight.getPower());
     }
 
     @Override
@@ -73,89 +79,52 @@ public class Drive extends SubSystem {
 
     }
 
-    public void runDrive(DriveControls driveType, double driveSpeed, double strafeSpeed, double turnSpeed, double rightY, double leftY) {
-        switch (driveType) {
-            case ARCADE:
-                mecanumDrive.setWeightedDrivePower(
-                        new Pose2d(
-                                driveSpeed,
-                                -strafeSpeed,
-                                -turnSpeed
-                        )
-                );
-                break;
+//    public void runDrive(DriveControls driveType) {
+//        switch (driveType) {
+//            case ARCADE:
+//                xDrive.driveRobotCentric(
+//                        gamepad1.getLeftX(),
+//                        gamepad1.getLeftY(),
+//                        gamepad1.getRightY()
+//                );
+//                break;
+//
+//            default:
+//                driveType = DriveControls.ARCADE;
+//        }
+//    }
 
-            case TANK:
-                if (robot.gamepad1.right_bumper) {
-                    if (robot.gamepad1.right_trigger < 0.5) {
-                        strafe(0.5);
-                    }
-                    else if (robot.gamepad1.left_trigger < 0.5) {
-                        strafe(0.3);
-                    }
-                    else {
-                        strafe(1);
-                    }
-                }
-                else if (robot.gamepad1.left_bumper) {
-                    if (robot.gamepad1.right_trigger < 0.5) {
-                        strafe(-0.5);
-                    }
-                    else if (robot.gamepad1.left_trigger < 0.5) {
-                        strafe(-0.3);
-                    }
-                    else {
-                        strafe(-1);
-                    }
-                }
-                else {
-                    left(-leftY);
-                    right(-rightY);
-                }
-                break;
-
-            default:
-                driveType = DriveControls.TANK;
-        }
-    }
-
-    private void left(double power) {
-        try {
-            frontLeft.setPower(power);
-            backLeft.setPower(power);
-        } catch(Exception ex) {}
-    }
-
-    private void right(double power) {
-        try {
-            frontRight.setPower(power);
-            backRight.setPower(power);
-        } catch(Exception ex) {}
-    }
-
-    public void drive(double leftPower, double rightPower) {
-        left(leftPower);
-        right(rightPower);
-    }
-
-    public void strafe(double power) {
-        frontLeft.setPower(power);
-        backLeft.setPower(-power);
-        frontRight.setPower(-power);
-        backRight.setPower(power);
-    }
-
-    public void turn (double power) {
-
-        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        frontLeft.setPower(power);
-        backLeft.setPower(power);
-        frontRight.setPower(-power);
-        backRight.setPower(-power);
-    }
+//    private void left(double power) {
+//        try {
+//            frontLeft.set(power);
+//            backLeft.set(power);
+//        } catch(Exception ex) {}
+//    }
+//
+//    private void right(double power) {
+//        try {
+//            frontRight.set(power);
+//            backRight.set(power);
+//        } catch(Exception ex) {}
+//    }
+//
+//    public void drive(double leftPower, double rightPower) {
+//        left(leftPower);
+//        right(rightPower);
+//    }
+//
+//    public void strafe(double power) {
+//        frontLeft.set(power);
+//        backLeft.set(-power);
+//        frontRight.set(-power);
+//        backRight.set(power);
+//    }
+//
+//    public void turn (double power) {
+//        frontLeft.set(power);
+//        backLeft.set(power);
+//        frontRight.set(-power);
+//        backRight.set(-power);
+//    }
 
 }
