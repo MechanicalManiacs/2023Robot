ab1f8ca8af9b7609518366882d33e0ddec0dbde7
Saranga Rajagopalan <sarnga.raj@gmail.com>
2022-02-05 19:07:25 -0500

    pull it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/v1/fishlo/program/Competition/T265Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/v1/fishlo/program/Competition/T265Test.java
@@ -7,16 +7,19 @@ import com.arcrobotics.ftclib.geometry.Translation2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.fishlo.v1.fishlo.program.FishloAutonomousProgram;
+import org.firstinspires.ftc.teamcode.fishlo.v1.fishlo.robot.T265Manager;
 import org.firstinspires.ftc.teamcode.robot.Robot;
 
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.spartronics4915.lib.T265Camera;
 
 @Autonomous
 public class T265Test extends FishloAutonomousProgram {
 
     T265Camera apache;
-    T265Camera.CameraUpdate up;
-    double rad;
+    ElapsedTime timer = new ElapsedTime();
+    boolean free = false;
+
 
     @Override
     protected Robot buildRobot() {
@@ -26,39 +29,45 @@ public class T265Test extends FishloAutonomousProgram {
     @Override
     public void preMain() {
         telemetry.setAutoClear(true);
-        apache = new T265Camera(new Transform2d(new Translation2d(-0.1143,0), new Rotation2d(0)), 0, hardwareMap.appContext);
-        apache.start();
-        telemetry.addLine("Ready for start");
-        telemetry.update();
-        rad = apache.getLastReceivedCameraUpdate().pose.getRotation().getRadians();
-        while (!isStarted()) {
-            if (rad > 0) {
-                rad -= 0.01;
-                telemetry.addData("Rad", rad);
-                telemetry.update();
-            } else if (rad < 0) {
-                rad += 0.01;
-                telemetry.addData("Rad", rad);
-                telemetry.update();
-            }
+        apache = T265Manager.get(hardwareMap);
+        while (!apache.isStarted()) {
+            apache.start();
         }
-        telemetry.addData("Rad", rad);
-        telemetry.update();
-        apache.setPose(new Pose2d(0, 0, new Rotation2d(rad)));
+        while (!isStarted()) {
+            telemetry.addData("Confidence", apache.getLastReceivedCameraUpdate().confidence);
+            telemetry.update();
+        }
+//        rad = apache.getLastReceivedCameraUpdate().pose.getRotation().getRadians();
+//        while (!isStarted()) {
+//            if (rad > 0) {
+//                rad -= 0.01;
+//                telemetry.addData("Rad", rad);
+//                telemetry.update();
+//            } else if (rad < 0) {
+//                rad += 0.01;
+//                telemetry.addData("Rad", rad);
+//                telemetry.update();
+//            }
+//        }
+//        telemetry.addData("Rad", rad);
+//        telemetry.update();
+        apache.setPose(new Pose2d(0, 0, new Rotation2d(0)));
     }
 
     @Override
     public void main() {
-        apache.setPose(new Pose2d(0,0,new Rotation2d(rad)));
-        telemetry.addLine("Camera heading 0");
-        telemetry.update();
         sleep(1000);
-        while (!isStopRequested()) {
-            telemetry.addData("PosX", apache.getLastReceivedCameraUpdate().pose.getX() * 39.3701 + 5);
-            telemetry.addData("PosY", apache.getLastReceivedCameraUpdate().pose.getY() * 39.3701 * 1.0625 - 72);
+        timer.reset();
+        while (timer.seconds() < 300 && free == false) {
+            telemetry.addData("PosX", apache.getLastReceivedCameraUpdate().pose.getX() * 39.3701);
+            telemetry.addData("PosY", apache.getLastReceivedCameraUpdate().pose.getY() * 39.3701 - 72);
             telemetry.addData("Deg", apache.getLastReceivedCameraUpdate().pose.getRotation().getDegrees());
             telemetry.addData("Rad", apache.getLastReceivedCameraUpdate().pose.getRotation().getRadians());
             telemetry.update();
+            if (isStopRequested()) {
+                apache.stop();
+                free = true;
+            }
         }
     }
 }
