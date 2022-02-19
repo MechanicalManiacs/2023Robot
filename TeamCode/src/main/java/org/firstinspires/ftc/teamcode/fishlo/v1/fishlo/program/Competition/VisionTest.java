a182ad9d1a46e6e007cfdb07595153e1444b0946
AvyTk <68242505+Avy-no-kami@users.noreply.github.com>
2022-02-18 19:40:17 -0500

    ok

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/v1/fishlo/program/Competition/VisionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/v1/fishlo/program/Competition/VisionTest.java
@@ -18,11 +18,6 @@ import org.openftc.easyopencv.OpenCvCameraRotation;
 public class VisionTest extends FishloAutonomousProgram {
 
     TSEDetectionPipeline.BarcodePosition placement;
-    //VisionPipeline mainPipeline;
-    TSEDetectionPipeline mainPipeline;
-    OpenCvCamera cam;
-    Scalar low = VisionPipeline.scalarLowerHSV;
-    Scalar high = VisionPipeline.scalarUpperHSV;
 
     @Override
     protected Robot buildRobot() {
@@ -32,35 +27,13 @@ public class VisionTest extends FishloAutonomousProgram {
     @Override
     public void preMain() {
         telemetry.addLine("start");
+        telemetry.update();
         telemetry.setAutoClear(true);
+        vision.initVision();
         while (!isStarted()) {
-            int cameraViewID = hardwareMap.appContext.getResources().getIdentifier("cameraMoniterViewId", "id", hardwareMap.appContext.getPackageName());
-            cam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraViewID);
-
-            cam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
-                @Override
-                public void onOpened() {
-                    //mainPipeline = new VisionPipeline(0, 0, 0, 0);
-                    mainPipeline = new TSEDetectionPipeline();
-                    cam.setPipeline(mainPipeline);
-//                    mainPipeline.configureScalarLower(low.val[0], low.val[1], low.val[2]);
-//                    mainPipeline.configureScalarLower(high.val[0], high.val[1], high.val[2]);
-
-                    cam.startStreaming(640, 360, OpenCvCameraRotation.UPRIGHT);
-                }
-
-                @Override
-                public void onError(int errorCode) {
-                    telemetry.addData("camera error", "");
-                    telemetry.update();
-                    System.exit(0);
-                }
-            });
             placement = vision.getPlacement();
             telemetry.addData("Position", placement);
             telemetry.update();
-//            telemetry.addData("Position", placement);
-//            telemetry.update();
         }
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/v1/fishlo/program/Competition/WareRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/v1/fishlo/program/Competition/WareRed.java
@@ -14,6 +14,7 @@ import org.firstinspires.ftc.teamcode.fishlo.v1.fishlo.program.FishloAutonomousP
 import org.firstinspires.ftc.teamcode.fishlo.v1.fishlo.robot.Intake;
 import org.firstinspires.ftc.teamcode.fishlo.v1.fishlo.robot.TSEDetectionPipeline;
 import org.firstinspires.ftc.teamcode.robot.Robot;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 import java.util.Vector;
 
@@ -40,12 +41,12 @@ public class WareRed extends FishloAutonomousProgram {
         telemetry.update();
         timer = new ElapsedTime();
         drive.initGyro();
-//        vision.initVision();
-//        while (!isStarted()) {
-//            position = vision.getPlacement();
-//            telemetry.addData("Position", position);
-//            telemetry.update();
-//        }
+        vision.initVision();
+        while (!isStarted()) {
+            position = vision.getPlacement();
+            telemetry.addData("Position", position);
+            telemetry.update();
+        }
     }
 
     // strafe right is positive power, strafe left is negative power!
@@ -55,13 +56,12 @@ public class WareRed extends FishloAutonomousProgram {
     public void main() {
         telemetry.addLine("I hate ftc");
         telemetry.update();
-        //vision.stop();
+        vision.stop();
         telemetry.clear();
         mdrive = new SampleMecanumDrive(hardwareMap);
         //sleep(200);
         mdrive.setPoseEstimate(new Pose2d());
 
-        position = TSEDetectionPipeline.BarcodePosition.CENTER;
         Pose2d start_pose = new Pose2d(0, 0, Math.toRadians(0));
 
         switch (position) {
@@ -83,7 +83,7 @@ public class WareRed extends FishloAutonomousProgram {
         telemetry.addLine("Before trajectory");
 
         Trajectory to_hub  = mdrive.trajectoryBuilder(start_pose)
-                .splineToConstantHeading(new Vector2d(17, 25), Math.toRadians(0))
+                .splineToConstantHeading(new Vector2d(19, 25), Math.toRadians(0))
                 .build();
         telemetry.addLine("After");
         mdrive.followTrajectory(to_hub);
@@ -114,51 +114,43 @@ public class WareRed extends FishloAutonomousProgram {
         telemetry.addLine("i stg part 2");
         telemetry.update();
         intake.intake(Intake.IntakeState.ON);
-        Pose2d warePose = new Pose2d(ware.end().getX(), ware.end().getY(), ware.end().getHeading());
-        Trajectory work = mdrive.trajectoryBuilder(warePose)
-                .splineToConstantHeading(new Vector2d(-15, -35), Math.toRadians(-89),
-                        SampleMecanumDrive.getVelocityConstraint(6, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+        Pose2d turn_pose = new Pose2d(ware.end().getX(), ware.end().getY(), Math.toRadians(-90));
+        Trajectory Test = mdrive.trajectoryBuilder(turn_pose)
+                .splineToConstantHeading(new Vector2d(-15, -30), Math.toRadians(-90))
+                .addDisplacementMarker(() -> {
+                    intake.intake(Intake.IntakeState.ON);
+
+                })
+                .splineToConstantHeading(new Vector2d(-15, -38), Math.toRadians(-90),
+                        SampleMecanumDrive.getVelocityConstraint(8, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
                         SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
-                .splineToConstantHeading(new Vector2d(-15, 18), Math.toRadians(-90))
+
                 .build();
+
         telemetry.addLine("built the alt");
         telemetry.update();
-        mdrive.followTrajectory(work);
+        mdrive.followTrajectory(Test);
 
-
-        Trajectory just_work = mdrive.trajectoryBuilder(ware.end())
-                //sa
-                .build();
         telemetry.addLine("about to start new trajectory");
         telemetry.update();
-        mdrive.followTrajectory(just_work);
-        telemetry.addLine("finished trajectory");
+        Trajectory block_place = mdrive.trajectoryBuilder(Test.end())
+                .splineTo(new Vector2d(-15, 20), Math.toRadians(-90))
+                .build();
+        telemetry.addLine("Built Trajectory block_place");
         telemetry.update();
-//        Trajectory place_block = mdrive.trajectoryBuilder(ware.end())
-//                .strafeLeft(20)
-//                .build();
-//
-//
-//
-//
-//
-//
-//        mdrive.followTrajectory(place_block);
-//        mdrive.turn(78);
-//
-//
-//
-//        Trajectory getBlock = mdrive.trajectoryBuilder(ware.end())
-//                .splineTo(new Vector2d(0,-30), Math.toRadians(-180))
-//                .addDisplacementMarker(() -> {
-//                    intake.intake(Intake.IntakeState.ON);
-//
-//                })
-//                .splineTo(new Vector2d(0, -40), Math.toRadians(-90),
-//                        SampleMecanumDrive.getVelocityConstraint(8, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
-//                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
-//                .build();
-//        mdrive.followTrajectory(getBlock);
+        mdrive.followTrajectory(block_place);
+        telemetry.addLine("Finished running Trajectory block_place");
+        telemetry.update();
+        Trajectory strafe_left = mdrive.trajectoryBuilder(block_place.end())
+                .strafeLeft(5)
+                .build();
+        telemetry.addLine("built strafing");
+        telemetry.update();
+        mdrive.followTrajectory(strafe_left);
+        telemetry.addLine("Finished strafing");
+        telemetry.update();
+
+
 
 
 
